87355a4b23c47e231af6aeaf2c86c0bd1e4d6a24
joaopedropitzz <joao.p2007@aluno.ifsc.edu.br>
2025-11-11 11:08:28 -0300

    php

--- index.php
+++ index.php
@@ -19,5 +19,43 @@
             echo "<p>".$a+$b."</p>";
         ?>
         <p><?php echo $a+$b ?> </p>
+        <h2>Operador ternario</h2>
+        <?php
+        $situacao = $nota >= 6 ? 'aprovado' : 'reprovado';
+        echo $situacao
+        ?>
+        <h2> while </h2>
+        <?php
+        $i = 0;
+        while($i<10){
+            $i++;
+            echo "i=".$i." - ";
+        }
+        $vetor = array(20,19,22,19,23);
+        for ($posicao = 0; $posicao <5; $posicao++){
+            echo $vetor[$posicao] . '<br />';
+        }
+        ?>
+        <h2>Array em php</h2>
+        <?php
+        $vetor = array(2,10,20,30);
+        $posicao=0;
+        
+        ?>
+        <?php
+        $vetor = array(
+            'nome' => 'Guilherme',
+            'sobrenome' => 'Silva',
+            'cpf' => '123.456.789.00'
+        );
+        echo 'Nome: ' . $vetor['nome'] . '<br/>';
+        echo 'Sobrenome: ' . $vetor['sobrenome']. '<br />';
+        echo 'CPF: ' . $vetor['cpf'];
+        ?>
+        <h2>Variaveis de ambiente em PHP</h2>
+        <?php
+        echo "$_GET".$_GET['nome']."<br>";
+        echo "SERVER". var_dump ($_SERVER);   
+        ?>
 </body>
 </html>
